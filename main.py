72ac07d65986ab5e47d011af6bacfcb4fffd9fbe
Kyle Aoki <kyle.aoki.500@gmail.com>
2023-08-09 17:51:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,18 +12,19 @@ from tessie import TessieApi
 from util import has_arg
 
 mutex = Lock()
-tesla_ac_activation_key = keyboard.Key.f12
+tesla_ac_activation_key = None
 tessie_api = None
-ac_duration_seconds = 20
+ac_duration_seconds = None
 
 
 def climate_sequence(key):
-    if mutex.locked():
-        log.info("mutex locked, ignoring ac activation key press")
-        return
-    if key == tesla_ac_activation_key:
+    if str(key) == tesla_ac_activation_key:
         log.info("tesla ac activation key pressed")
+        if mutex.locked():
+            log.info("mutex locked, ignoring ac activation key press")
+            return
         mutex.acquire()
+        log.info("starting start/stop climate sequence")
         if not tessie_api.is_awake():
             tessie_api.wake_up()
         tessie_api.start_climate_control()
@@ -43,7 +44,13 @@ def program_configure():
     if not cfg_file:
         raise Exception("must supply config file: python3 main.py <path-to-cfg-file>")
     cfg = yaml.safe_load(Path(cfg_file).read_text())
-    cfg_file_properies = ["host", "vin", "access_token"]
+    cfg_file_properies = [
+        "host",
+        "vin",
+        "access_token",
+        "button_code",
+        "ac_duration_seconds",
+    ]
     for p in cfg_file_properies:
         if p not in cfg:
             raise Exception(f"did not find {p} in config file")
@@ -51,12 +58,17 @@ def program_configure():
 
 
 def main():
-    global tessie_api
+    global tessie_api, tesla_ac_activation_key, ac_duration_seconds
     log.info("running tesla-ac-button program")
     log.info(f"received program arguments: {sys.argv[1:]}")
 
     cfg = program_configure()
+    
     tessie_api = TessieApi(cfg["host"], cfg["vin"], cfg["access_token"])
+    tesla_ac_activation_key = cfg["button_code"]
+    ac_duration_seconds = cfg["ac_duration_seconds"]
+
+    log.info(f"running with button {tesla_ac_activation_key} and ac duration {ac_duration_seconds}s")
 
     fn(has_arg("is_awake"), tessie_api.is_awake)
     fn(has_arg("wake"), tessie_api.wake_up)
